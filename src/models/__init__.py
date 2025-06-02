5a86bfeb216e2d1488dd61fee837c102a80b9c53
Florentin Kybaloo TCHANGAI <tchangaiflorentin6@gmail.com>
2025-06-02 10:33:23 +0000

    Ajout du fichier __init__.py pour initialiser le module des modèles de données

---
+++ src/models/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module contenant les modèles de données de l'application
+"""
+
+from .conversation import Conversation, Message
+from .user import User
+from .ai_model import AIModel
+
+__all__ = ['Conversation', 'Message', 'User', 'AIModel']
